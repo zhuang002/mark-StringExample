17a201089b655747994d992afa7d880e5f0a71b4
zhuang002 <zhuang001@gmail.com>
2021-02-09 17:29:20 -0500

    first time

---
+++ StringExample/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="I am a student";
+		
+		// iterate all characters in a string
+		for (int i=0;i<s.length();i++) {
+			char c=s.charAt(i);
+			System.out.print(c+",");
+		}
+		System.out.println();
+	}
+
+}
